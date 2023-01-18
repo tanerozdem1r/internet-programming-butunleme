f31b385f1cafeef761bf58a4cfe7dcf1cc6fd232
Taner <114864135+tanerozdem1r@users.noreply.github.com>
2023-01-18 17:48:51 +0300

    butunleme
    xxx

--- Shop/admin/include/inc_pageSecurity.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include '../../taslak.php';
-//İzinsiz girişleri önleme
-if ($_SESSION["giris"] != sha1(md5("var")) || $_COOKIE["kullanici"] != "msb") {
-    header("Location: logout.php");
-}
-
-?>
--- Shop/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_destroy();
-session_unset();
-unset($_SESSION['oturum']);
-echo "<script> window.location.href='index.php'</script>";
-setcookie("kullanici","",time()-1);
-
-?>
